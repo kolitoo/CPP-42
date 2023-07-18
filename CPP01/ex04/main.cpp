ec889dda97a1af6d0468becf011d4ccc8fcfc66d
alex <abourdon@paul-f4Br7s2.clusters.42paris.fr>
2023-07-18 19:23:13 +0200

    sed

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abourdon <abourdon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/17 16:59:18 by abourdon          #+#    #+#             */
-/*   Updated: 2023/07/17 17:36:11 by abourdon         ###   ########.fr       */
+/*   Updated: 2023/07/18 19:21:46 by abourdon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,24 @@ int main(int ac, char **av)
 {
     if (ac == 4)
     {
+        std::string s1 = av[2];
+        std::string s2 = av[3];
         std::fstream file(av[1]);
-        if (file != NULL)
+        if (file.is_open() != 0)
         {
             std::string line;
+            std::string content;
             while (std::getline(file, line))
-                std::cout << line << std::endl;
+                content.append(line + '\n');
+            std::size_t i = content.find(s1);
+            while (i != std::string::npos)
+            {
+                content.erase(i, s1.size());
+                content.insert(i, s2);
+                i = content.find(s1, i + s2.size());
+            }
             file.close();
+            std::cout << content << std::endl;
         }
         else
         {
