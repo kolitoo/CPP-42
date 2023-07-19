4d957235efe5201c3a2ea4fb5d6592b61956f4f2
alex <abourdon@paul-f4Br8s3.clusters.42paris.fr>
2023-07-19 18:54:51 +0200

    phonebook upgrade

--- CPP00/ex01/Phonebook.hpp
+++ CPP00/ex01/Phonebook.hpp
@@ -6,7 +6,7 @@
 /*   By: abourdon <abourdon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 21:24:08 by abourdon          #+#    #+#             */
-/*   Updated: 2023/07/19 17:34:20 by abourdon         ###   ########.fr       */
+/*   Updated: 2023/07/19 18:52:54 by abourdon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,8 @@ class	PhoneBook
 	private:
 		Contact _Contact[8];
 		std::string	find_put_info(std::string info);
-		void	_print_book(void);
+		void	_print_book(void) const;
+		void	_print_contact_info(std::string const str) const;
 		int	_i;
 		
 };
--- CPP00/ex01/phonebook.cpp
+++ CPP00/ex01/phonebook.cpp
@@ -6,7 +6,7 @@
 /*   By: abourdon <abourdon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 21:22:47 by abourdon          #+#    #+#             */
-/*   Updated: 2023/07/19 17:53:09 by abourdon         ###   ########.fr       */
+/*   Updated: 2023/07/19 18:53:00 by abourdon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,8 @@ std::string	PhoneBook::find_put_info(std::string info)
 
 void	PhoneBook::add(void)
 {
+	if (this->_i == 8)
+		this->_i = 0;
 	this->_Contact[this->_i].set_index(this->_i);
 	this->_Contact[this->_i].set_firstname(this->find_put_info("First Name ? "));
 	this->_Contact[this->_i].set_lastname(this->find_put_info("Last Name ? "));
@@ -48,18 +50,28 @@ void	PhoneBook::add(void)
 	return ;
 }
 
-void	PhoneBook::_print_book(void)
+void	PhoneBook::_print_contact_info(std::string const str) const
+{
+	std::cout << "|";
+	if (str.size() > 10)
+		std::cout << str.substr(0,9) << ".";
+	else if (str.size() == 10)
+		std::cout << str;
+	else
+		std::cout << std::setw(10) << str;
+}
+
+void	PhoneBook::_print_book(void) const
 {
 	for(int i = 0; i < 8; i++)
 	{
 		if (this->_Contact[i].get_firstname().empty() == 1)
 			return ;
-		std::cout << "|----------|----------|----------|----------|" << std::endl;
-		std::cout << "|" << std::setw(10) << this->_Contact[i].get_index();
-		std::cout << "|" << std::setw(10) << this->_Contact[i].get_firstname();
-		std::cout << "|" << std::setw(10) << this->_Contact[i].get_lastname();
-		std::cout << "|" << std::setw(10) << this->_Contact[i].get_nickname() << "|" << std::endl;
-		std::cout << "|----------|----------|----------|----------|" << std::endl;
+		std::cout << "|" << std::setw(9) << this->_Contact[i].get_index();
+		this->_print_contact_info(this->_Contact[i].get_firstname());
+		this->_print_contact_info(this->_Contact[i].get_lastname());
+		this->_print_contact_info( this->_Contact[i].get_nickname());
+	 	std::cout << "|\n";
 	}
 }
 
@@ -72,7 +84,7 @@ void	PhoneBook::search(void)
 		return ;
 	}
 	this->_print_book();
-	std::cout << "|            Press Contact Number" << std::setw(12) << "|" << std::endl;
+	std::cout << "|            Press Contact Number" << std::setw(11) << "|" << std::endl;
 	while (1)
 	{
 		std::cin >> index;
@@ -80,13 +92,13 @@ void	PhoneBook::search(void)
 			std::cout << "Number contact unknown, press a correct number" << std::endl;
 		else
 		{
-			std::cout << "|-------------------------------------------|" << std::endl;
+			std::cout << "|------------------------------------------|" << std::endl;
 			std::cout << "   First name:             " << this->_Contact[index[0] - 48].get_firstname() << std::endl;
 			std::cout << "   Last name:              " <<  this->_Contact[index[0] - 48].get_lastname() << std::endl;
 			std::cout << "   Nick name:              " << this->_Contact[index[0] - 48].get_nickname() << std::endl;
 			std::cout << "   Phone number:           " <<  this->_Contact[index[0] - 48].get_phonenumber() << std::endl;
 			std::cout << "   Darkest_secret:         " <<  this->_Contact[index[0] - 48].get_darkestsecret() << std::endl;
-			std::cout << "|-------------------------------------------|" << std::endl;
+			std::cout << "|------------------------------------------|" << std::endl;
 			break;
 		}
 	}
